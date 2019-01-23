7821dc31d777f8171ec0933b34a79ed64e29f2c6
Arnav Buch <abuch99@gmail.com>
2019-01-24 03:01:12 +0530

    Admin Docs Fragment Made

--- app/src/main/java/in/ac/bits_hyderabad/swd/swd/admin/fragment/Admin_DocGenFragment.java
+++ app/src/main/java/in/ac/bits_hyderabad/swd/swd/admin/fragment/Admin_DocGenFragment.java
@@ -1,6 +1,26 @@
 package in.ac.bits_hyderabad.swd.swd.admin.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.ac.bits_hyderabad.swd.swd.R;
 
 public class Admin_DocGenFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        View view = inflater.inflate(R.layout.admin_doc_fragment, parent, false);
+        return view;
+    }
+}
 }
