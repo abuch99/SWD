6703b3821441261cae7fb52b0b6c7c7a4c61fa95
Arnav Buch <abuch99@gmail.com>
2019-06-06 20:40:47 +0530

    Mess web_view added

--- app/src/main/java/in/ac/bits_hyderabad/swd/swd/user/fragment/User_MessFragment.java
+++ app/src/main/java/in/ac/bits_hyderabad/swd/swd/user/fragment/User_MessFragment.java
@@ -1,15 +1,27 @@
 package in.ac.bits_hyderabad.swd.swd.user.fragment;
 
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import in.ac.bits_hyderabad.swd.swd.R;
 
 public class User_MessFragment extends Fragment {
+
+    WebView mess_web;
+    String url = "http://swd.bits-hyderabad.ac.in/mess";
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +31,35 @@ public class User_MessFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
         View view = inflater.inflate(R.layout.mess_fragment, parent, false);
+
+        mess_web = (WebView) view.findViewById(R.id.mess_web);
+        mess_web.loadUrl(url);
+        mess_web.getSettings().setJavaScriptEnabled(true);
+        mess_web.setWebViewClient(new MyWebViewClient());
+
+
         return view;
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            final Uri uri = Uri.parse(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
     }
 }
