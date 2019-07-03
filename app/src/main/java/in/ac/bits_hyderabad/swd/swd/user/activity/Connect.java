e6ddc126aaf2eec9f88b742a8102031b94551570
Prasoon Baghel <prasoon040999@gmail.com>
2019-07-03 12:47:50 +0530

    updated goodies card margins

--- app/src/main/java/in/ac/bits_hyderabad/swd/swd/user/activity/Connect.java
+++ app/src/main/java/in/ac/bits_hyderabad/swd/swd/user/activity/Connect.java
@@ -87,11 +87,11 @@ public class Connect extends AppCompatActivity implements PersonAdapter.itemClic
 
 
         adapter.addFragment(new SWD_ConnectFragment(),"SWD");
-        adapter.addFragment(new SU_ConnectFragment(),"SU");
+        adapter.addFragment(new SU_ConnectFragment(),"SUC");
         adapter.addFragment(new CRC_ConnectFragment(),"CRC");
         adapter.addFragment(new SMC_ConnectFragment(),"SMC");
         adapter.addFragment(new EC_ConnectFragment(),"EC");
-        viewPager.setOffscreenPageLimit(3);
+        viewPager.setOffscreenPageLimit(4);
         viewPager.setAdapter(adapter);
     }
 
