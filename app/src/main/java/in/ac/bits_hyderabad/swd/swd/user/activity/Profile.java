e7ed49adf34e3cfe62195ceb3ae03a2663c55a58
Aryan <aryan.arora180@gmail.com>
2020-03-08 21:24:38 +0530

    Redesigned the ProfileActivity using Material components

--- app/src/main/java/in/ac/bits_hyderabad/swd/swd/user/activity/Profile.java
+++ app/src/main/java/in/ac/bits_hyderabad/swd/swd/user/activity/Profile.java
@@ -2,26 +2,16 @@ package in.ac.bits_hyderabad.swd.swd.user.activity;
 
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -30,6 +20,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,8 +36,7 @@ public class Profile extends AppCompatActivity {
 
 
     //UserTable mUserTable;
-    EditText etName,etRoom,etPhn,etId,etDOB,etGender,etNat,etEmail,etBlood,etAddress,etCity,etState,etAadhaar,etPan,etfcomp,etfdesg,etmcomp,etmdesg,etmed_history,etcurrent_med,etCategory
-            ,etFname,etFphn,etFemail,etFocc,etMname,etMphn,etMemail,etMocc,etBank,etAccNo,etIfsc;
+    TextInputEditText etName, etRoom, etPhn, etId, etDOB, etGender, etNat, etEmail, etBlood, etAddress, etCity, etState, etAadhaar, etPan, etfcomp, etfdesg, etmcomp, etmdesg, etmed_history, etcurrent_med, etCategory, etFname, etFphn, etFemail, etFocc, etMname, etMphn, etMemail, etMocc, etBank, etAccNo, etIfsc;
     ActionBar actionBar;
     FloatingActionButton fabUpdate;
     String uid,password;
@@ -56,20 +47,18 @@ public class Profile extends AppCompatActivity {
     Boolean successfull=false;
     ProgressDialog dialog;
     RequestQueue queue;
-    LinearLayout llProfile;
-    SwipeRefreshLayout srProfile;
 
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_profile_redesigned);
 
         Toolbar toolbar=findViewById(R.id.profile_toolbar);
         setSupportActionBar(toolbar);
         actionBar=getSupportActionBar();
 
-        dialog =new ProgressDialog(this);
+        dialog = new ProgressDialog(this);
         dialog.setMessage("Loading");
         dialog.setCanceledOnTouchOutside(false);
         dialog.setCancelable(false);
@@ -79,17 +68,14 @@ public class Profile extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
-
-        prefs=getApplicationContext().getSharedPreferences("USER_LOGIN_DETAILS",MODE_PRIVATE);
-        uid=prefs.getString("uid",null);
-        password=prefs.getString("password",null);
+        prefs = getApplicationContext().getSharedPreferences("USER_LOGIN_DETAILS", MODE_PRIVATE);
+        uid = prefs.getString("uid", null);
+        password = prefs.getString("password", null);
         initialize_views();
 
-        llProfile.setVisibility(View.GONE);
         //mUserTable=new UserTable(getApplicationContext());
-        srProfile.setRefreshing(true);
         reload();
-        queue= Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
 
         fabUpdate=findViewById(R.id.fabUpdate);
         fabUpdate.setEnabled(false);
@@ -105,7 +91,7 @@ public class Profile extends AppCompatActivity {
                 else
                 {
                     try {
-                        JSONObject updatedObj=getupdatedJSONObject(object);
+                        JSONObject updatedObj = getupdatedJSONObject(object);
                         updatetoServer(updatedObj);
                     }
                     catch (JSONException e){
@@ -113,16 +99,6 @@ public class Profile extends AppCompatActivity {
                     }
 
                 }
-            }
-        });
-
-        srProfile.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                llProfile.setVisibility(View.GONE);
-                fabUpdate.setImageResource(R.drawable.ic_update_details);
-                updating=false;
-                reload();
             }
         });
 
@@ -137,7 +113,6 @@ public class Profile extends AppCompatActivity {
             public void onResponse(String response) {
 
                 try {
-
                     object = new JSONObject(response);
                     if (!object.getBoolean("error")) {
                         try {
@@ -147,7 +122,6 @@ public class Profile extends AppCompatActivity {
                             fabUpdate.setEnabled(false);
                         }
                         disable_EditText();
-                        llProfile.setVisibility(View.VISIBLE);
                         fabUpdate.setEnabled(true);
                     } else {
                         Toast.makeText(Profile.this, "Something went wrong!!", Toast.LENGTH_SHORT).show();
@@ -159,15 +133,12 @@ public class Profile extends AppCompatActivity {
                     Toast.makeText(Profile.this, "Something went wrong!!", Toast.LENGTH_SHORT).show();
                     fabUpdate.setEnabled(false);
                 }
-
-                srProfile.setRefreshing(false);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(Profile.this, "Please check your Internet connection!", Toast.LENGTH_SHORT).show();
                 fabUpdate.setEnabled(false);
-                srProfile.setRefreshing(false);
             }
         }) {
             @Override
@@ -188,7 +159,8 @@ public class Profile extends AppCompatActivity {
 
 
     }
-    public void setData(JSONObject object)throws JSONException {
+
+    public void setData(JSONObject object) throws JSONException {
         etName.setText(object.getString("name"));
         etRoom.setText(object.getString("room"));
         etPhn.setText(object.getString("phone"));
@@ -257,63 +229,6 @@ public class Profile extends AppCompatActivity {
         etmdesg.setEnabled(false);
         etmed_history.setEnabled(false);
         etcurrent_med.setEnabled(false);
-
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            etRoom.setTextColor(getColor(R.color.colorAccent));
-            etPhn.setTextColor(getColor(R.color.colorAccent));
-            etIfsc.setTextColor(getColor(R.color.colorAccent));
-            etAccNo.setTextColor(getColor(R.color.colorAccent));
-            etMocc.setTextColor(getColor(R.color.colorAccent));
-            etMphn.setTextColor(getColor(R.color.colorAccent));
-            etMemail.setTextColor(getColor(R.color.colorAccent));
-            etState.setTextColor(getColor(R.color.colorAccent));
-            etCity.setTextColor(getColor(R.color.colorAccent));
-            etAddress.setTextColor(getColor(R.color.colorAccent));
-            etEmail.setTextColor(getColor(R.color.colorAccent));
-            etFocc.setTextColor(getColor(R.color.colorAccent));
-            etFemail.setTextColor(getColor(R.color.colorAccent));
-            etFphn.setTextColor(getColor(R.color.colorAccent));
-            etBank.setTextColor(getColor(R.color.colorAccent));
-            etAadhaar.setTextColor(getColor(R.color.colorAccent));
-            etPan.setTextColor(getColor(R.color.colorAccent));
-            etCategory.setTextColor(getColor(R.color.colorAccent));
-            etfcomp.setTextColor(getColor(R.color.colorAccent));
-            etfdesg.setTextColor(getColor(R.color.colorAccent));
-            etmcomp.setTextColor(getColor(R.color.colorAccent));
-            etmdesg.setTextColor(getColor(R.color.colorAccent));
-            etmed_history.setTextColor(getColor(R.color.colorAccent));
-            etcurrent_med.setTextColor(getColor(R.color.colorAccent));
-
-
-        }
-        etRoom.setBackground(getDrawable(R.drawable.edit_text));
-        etPhn.setBackground(getDrawable(R.drawable.edit_text));
-        etIfsc.setBackground(getDrawable(R.drawable.edit_text));
-        etAccNo.setBackground(getDrawable(R.drawable.edit_text));
-        etMocc.setBackground(getDrawable(R.drawable.edit_text));
-        etMphn.setBackground(getDrawable(R.drawable.edit_text));
-        etEmail.setBackground(getDrawable(R.drawable.edit_text));
-        etMemail.setBackground(getDrawable(R.drawable.edit_text));
-        etState.setBackground(getDrawable(R.drawable.edit_text));
-        etCity.setBackground(getDrawable(R.drawable.edit_text));
-        etAddress.setBackground((getDrawable(R.drawable.edit_text)));
-        etFocc.setBackground((getDrawable(R.drawable.edit_text)));
-        etFemail.setBackground((getDrawable(R.drawable.edit_text)));
-        etFphn.setBackground(getDrawable(R.drawable.edit_text));
-        etBank.setBackground(getDrawable(R.drawable.edit_text));
-        etAadhaar.setBackground(getDrawable(R.drawable.edit_text));
-        etPan.setBackground(getDrawable(R.drawable.edit_text));
-        etCategory.setBackground(getDrawable(R.drawable.edit_text));
-        etfcomp.setBackground(getDrawable(R.drawable.edit_text));
-        etfdesg.setBackground(getDrawable(R.drawable.edit_text));
-        etmcomp.setBackground(getDrawable(R.drawable.edit_text));
-        etmdesg.setBackground(getDrawable(R.drawable.edit_text));
-        etmed_history.setBackground(getDrawable(R.drawable.edit_text));
-        etcurrent_med.setBackground(getDrawable(R.drawable.edit_text));
-
-
     }
     public void onEditClick() {
 
@@ -341,87 +256,8 @@ public class Profile extends AppCompatActivity {
         etmed_history.setEnabled(true);
         etcurrent_med.setEnabled(true);
 
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            etRoom.setTextColor(getColor(R.color.black));
-            etPhn.setTextColor(getColor(R.color.black));
-            etIfsc.setTextColor(getColor(R.color.black));
-            etAccNo.setTextColor(getColor(R.color.black));
-            etMocc.setTextColor(getColor(R.color.black));
-            etMphn.setTextColor(getColor(R.color.black));
-            etMemail.setTextColor(getColor(R.color.black));
-            etState.setTextColor(getColor(R.color.black));
-            etCity.setTextColor(getColor(R.color.black));
-            etAddress.setTextColor(getColor(R.color.black));
-            etEmail.setTextColor(getColor(R.color.black));
-            etFocc.setTextColor(getColor(R.color.black));
-            etFemail.setTextColor(getColor(R.color.black));
-            etFphn.setTextColor(getColor(R.color.black));
-            etBank.setTextColor(getColor(R.color.black));
-            etAadhaar.setTextColor(getColor(R.color.black));
-            etPan.setTextColor(getColor(R.color.black));
-            etfcomp.setTextColor(getColor(R.color.black));
-            etfdesg.setTextColor(getColor(R.color.black));
-            etmcomp.setTextColor(getColor(R.color.black));
-            etmdesg.setTextColor(getColor(R.color.black));
-            etmed_history.setTextColor(getColor(R.color.black));
-            etcurrent_med.setTextColor(getColor(R.color.black));
-
-        }
-
-
-        etRoom.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etPhn.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etIfsc.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etAccNo.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etMocc.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etMphn.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etEmail.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etMemail.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etState.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etCity.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etAddress.setBackground((getDrawable(R.drawable.edit_text_selected)));
-        etFocc.setBackground((getDrawable(R.drawable.edit_text_selected)));
-        etFemail.setBackground((getDrawable(R.drawable.edit_text_selected)));
-        etFphn.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etBank.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etAadhaar.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etPan.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etfcomp.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etfdesg.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etmcomp.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etmdesg.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etcurrent_med.setBackground(getDrawable(R.drawable.edit_text_selected));
-        etmed_history.setBackground(getDrawable(R.drawable.edit_text_selected));
-
-
-        etRoom.setFocusable(true);
-        etPhn.setFocusable(true);
-        etIfsc.setFocusable(true);
-        etAccNo.setFocusable(true);
-        etMocc.setFocusable(true);
-        etMphn.setFocusable(true);
-        etMemail.setFocusable(true);
-        etState.setFocusable(true);
-        etCity.setFocusable(true);
-        etAddress.setFocusable(true);
-        etEmail.setFocusable(true);
-        etFocc.setFocusable(true);
-        etFemail.setFocusable(true);
-        etFphn.setFocusable(true);
-        etBank.setFocusable(true);
-        etAadhaar.setFocusable(true);
-        etPan.setFocusable(true);
-        etfcomp.setFocusable(true);
-        etfdesg.setFocusable(true);
-        etmcomp.setFocusable(true);
-        etmdesg.setFocusable(true);
-        etcurrent_med.setFocusable(true);
-        etmed_history.setFocusable(true);
-
         etRoom.requestFocus();
-        InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
         imm.showSoftInput(etRoom, InputMethodManager.SHOW_IMPLICIT);
 
     }
@@ -444,8 +280,6 @@ public class Profile extends AppCompatActivity {
 //      "branch":"","aadhar":null,"pan_card":"EPBPB8351D","category":"General","fcomp":"Indian Army","fdesg":"Ex Havaldar","mcomp":"NIL","mdesg":"NIL",
 //      "localadd":"NIL","guardian":"NIL","gphone":"NIL","med_history":"NIL","current_med":"NIL",
 
-        llProfile=findViewById(R.id.llProfile);
-        srProfile=findViewById(R.id.srProfile);
         etName=findViewById(R.id.etName);
         etRoom=findViewById(R.id.etRoom);
         etPhn=findViewById(R.id.etPhn);
